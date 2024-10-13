5a2c3de294efc1cd8923c43d6c5a921726eadd2e
Zaytsev Mikhail <mivlz@mail.ru>
2024-10-13 17:44:53 +0300

    Добавлена функция редактирования текста задачи

--- 6. Домашнее задание по модулю №5.1 JS база+/script.js
+++ 6. Домашнее задание по модулю №5.1 JS база+/script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
   loadTasks();
 });
 
+const tasksList = document.querySelector('#tasksList');
+
 function loadTasks(){
   const name = JSON.parse(localStorage.getItem('name')) || [];
   const taskList = document.getElementById('taskList');
@@ -13,14 +15,23 @@ function loadTasks(){
     li.innerHTML = `
       <span>${task}</span>
       <div>
-      <input type="checkbox"></input>
+      <button onclick="doneTask(this)">Выполнено</button>
       <button onclick="editTask(this)">Редактировать</button>
       <button onclick="deleteTask(this)">Удалить</button>
       </div>
       `;
     taskList.appendChild(li);
   })
-
+//Подгшружаем Чекбоксы в их отмеченном состоянии
+// const check = JSON.parse(localStorage.getItem('check')) || [];
+// console.log(check)
+//   check.forEach(checkbox => {
+//     const checks = JSON.parse(localStorage.getItem('check'))
+//     for (let i = 0; i < check.length ; i++) {
+//       checkbox = checks[i];
+//     }
+      
+//   })
 }
 
 function addTask() {
@@ -34,13 +45,39 @@ if (taskText === '') return;
   li.innerHTML = `
   <span>${taskText}</span>
   <div>
-  <input type="checkbox"></input>
+  <button onclick="doneTask(this)">Выполнено</button>
   <button onclick="editTask(this)">Редактировать</button>
   <button onclick="deleteTask(this)">Удалить</button>
   </div>
   `
   taskList.appendChild(li);
-
-
-taskInput.value = "";
+  saveTasks();
+  saveChecks();
+  taskInput.value = "";
 }
+
+function saveTasks() {
+  const taskList = document.getElementById('taskList');
+  const name = Array.from(taskList.children).map(li => li.querySelector('span').textContent);
+  localStorage.setItem('name',JSON.stringify(name));
+}
+
+function deleteTask(button) {
+  const li = button.parentElement.parentElement;
+  li.remove();
+  saveTasks();
+}
+
+function editTask(button) {
+  const li = button.parentElement.parentElement;
+  const taskText = li.querySelector('span').textContent;
+  const newTaskText = prompt("Редактировать задачу", taskText);
+
+  if(newTaskText !== null && newTaskText.trim() !== ""){
+    li.querySelector('span').textContent = newTaskText.trim();
+    saveTasks();
+
+}
+}
+
+
