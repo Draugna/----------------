bdd0e4eed04765e006ed34e5f3fbe30d493eac5a
Zaytsev Mikhail <mivlz@mail.ru>
2024-10-13 13:25:45 +0300

    Добавлен Скрипт по добавлению задач

---
+++ 6. Домашнее задание по модулю №5.1 JS база+/script.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+  loadTasks();
+});
+
+function loadTasks(){
+  const name = JSON.parse(localStorage.getItem('name')) || [];
+  const taskList = document.getElementById('taskList');
+
+
+//Добавляем строки
+  name.forEach(task => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <span>${task}</span>
+      <input type="checkbox"></input>
+      <button onclick="editTask(this)">Редактировать</button>
+      <button onclick="deleteTask(this)">Удалить</button>
+      `;
+    taskList.appendChild(li);
+  })
+
+}
+
+function addTask() {
+  const taskInput = document.getElementById('taskInput');
+  const taskText = taskInput.value.trim();
+
+if (taskText === '') return;
+
+  const taskList = document.getElementById('taskList');
+  const li = document.createElement('li');
+  li.innerHTML = `
+  <span>${taskText}</span>
+  <input type="checkbox"></input>
+  <button onclick="editTask(this)">Редактировать</button>
+  <button onclick="deleteTask(this)">Удалить</button>
+  `
+  taskList.appendChild(li);
+
+
+taskInput.value = "";
+}
